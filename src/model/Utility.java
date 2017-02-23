e173f9d7e8647100c63d9c12405d310f86f89a69
HYisen <alexhyisen@gmail.com>
2017-02-23 23:23:35 +0900

    fix the bug in the procedure dealing with rare charsets

--- src/model/Utility.java
+++ src/model/Utility.java
@@ -16,6 +16,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalTime;
 
 /**
@@ -24,6 +26,7 @@ import java.time.LocalTime;
  */
 public class Utility {
     static byte[] clean(String url){
+        //Utility.stamp("clean start");
         CleanerProperties props = new CleanerProperties();
 
         props.setTranslateSpecialEntities(true);
@@ -32,6 +35,7 @@ public class Utility {
 
         HtmlCleaner cleaner=new HtmlCleaner();
         TagNode tg;
+        //Utility.stamp("download 0");
         try {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             InputStream is = null;
@@ -51,24 +55,31 @@ public class Utility {
                 if (is != null) { is.close(); }
             }
             byte[] buf=baos.toByteArray();
+            //Utility.stamp("download 1");
 
             //cleaner.clean(new URL(url)) failed to get the correct charset.
             //page "http://www.fhxiaoshuo.com/read/67/67220/" as an example.
+            //Utility.stamp("clean 0");
             tg=cleaner.clean(new ByteArrayInputStream(buf));
+            //Utility.stamp("clean 1");
 
-            final String ATT="content";
-            String content=tg.findElementHavingAttribute(ATT,true).getAttributeByName(ATT);
+            //Utility.stamp("check 0");
             final String KEY="charset=";
+            String content=tg
+                    .findElementByAttValue("http-equiv","Content-Type",true,false)
+                    .getAttributeByName("content");
             String charset=content.substring(content.indexOf(KEY)+KEY.length());
             //System.out.println("charset = "+charset+" | "+props.getCharset());
             if(!"utf-8".equalsIgnoreCase(charset)){
                 tg=cleaner.clean(new ByteArrayInputStream(buf),charset);
             }
+            //Utility.stamp("check 1");
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
 
+        //Utility.stamp("output 0");
         ByteArrayOutputStream os=new ByteArrayOutputStream();//Maybe I should set a size, which is obviously larger than 100kb.
         try {
             new PrettyXmlSerializer(props).writeToStream(tg,os,"utf-8");
@@ -76,11 +87,22 @@ public class Utility {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+        //Utility.stamp("output 1");
         return os.toByteArray();
     }
 
     static void log(String msg){
         System.out.println(LocalTime.now()+" "+msg);
+    }
+
+    private static Instant record=null;
+    static void stamp(String msg){
+        if(record==null){
+            record=Instant.now();
+        }
+        long interval= Duration.between(record,Instant.now()).toMillis();
+        record=Instant.now();
+        System.out.printf("T+%4dms %s\n",interval,msg);
     }
 
     public static void main(String[] args) throws IOException {
