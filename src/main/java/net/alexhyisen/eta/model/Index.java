b4cc9ef42506c360a4b92d3bd5b0a1b34758bae6
HYisen <alexhyisen@gmail.com>
2017-09-30 17:55:36 +0900

    add the ability to parse index page that use <li>

--- src/main/java/net/alexhyisen/eta/model/Index.java
+++ src/main/java/net/alexhyisen/eta/model/Index.java
@@ -1,13 +1,18 @@
 package net.alexhyisen.eta.model;
 
-import org.htmlcleaner.CleanerProperties;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.*;
-import java.io.*;
+import javax.annotation.Nullable;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
@@ -17,6 +22,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Created by Alex on 2017/1/26.
@@ -24,9 +30,9 @@ import java.util.*;
  */
 public class Index {
     static class ddReader extends DefaultHandler {
-        String name=null;
-        String link=null;
-        boolean isValid=false;
+        String name = null;
+        String link = null;
+        boolean isValid = false;
         List<Hyperlink> data;
 
         //Those names come from the structure of the raw XML data.
@@ -38,58 +44,77 @@ public class Index {
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            name=localName;
+            name = localName;
             //System.out.println("start "+localName);
-            link=attributes.getValue("href");
-            if("dd".equals(name)){
-                isValid=true;
+            link = attributes.getValue("href");
+            //As for page such as http://www.33zw.com/xiaoshuo/49492/ , li rather than dd is used.
+            if ("dd".equals(name) || "li".equals(name)) {
+                isValid = true;
             }
         }
 
         @Override
         public void endElement(String uri, String localName, String qName) throws SAXException {
             //System.out.println("end "+localName);
-            if("a".equals(name)){
-                isValid=false;
+            if ("a".equals(name)) {
+                isValid = false;
             }
-            name=null;
-            link=null;
+            name = null;
+            link = null;
         }
 
         @Override
         public void characters(char[] ch, int start, int length) throws SAXException {
-            if(isValid&&Objects.equals(name,"a")&&link!=null){
-                String content=new String(ch,start,length);
-                data.add(new Hyperlink(content,link));
+            //System.out.println(isChapterLink(link) + "->" + link);
+            if (isValid && Objects.equals(name, "a") && isChapterLink(link)) {
+                String content = new String(ch, start, length);
+                data.add(new Hyperlink(content, link));
                 //System.out.println(content+"="+link);
             }
         }
 
+        private static Pattern pNumStr = Pattern.compile("\\d+");
+
+        //a chapter link usually ends with a number string filename.
+        private static boolean isChapterLink(@Nullable String orig) {
+            if (orig != null) {
+                int alpha = orig.lastIndexOf("/") + 1;
+                int omega = orig.lastIndexOf(".");
+                if (alpha < omega) {
+                    //System.out.println(orig.substring(alpha, omega));
+                    if (pNumStr.matcher(orig.substring(alpha, omega)).matches()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         @Override
         public InputSource resolveEntity(String publicId, String systemId) throws IOException, SAXException {
-            final String path=".";
+            final String path = ".";
             //Utility.log("solve p="+publicId+" s="+systemId);
-            String name=systemId.substring(systemId.lastIndexOf('/'));
-            if(!(Paths.get(path,name).toFile().exists())){
-                if(systemId.startsWith("http")){
-                    String prefix=systemId.substring(0,systemId.lastIndexOf('/'));
-                    Files.write(Paths.get(path,"prefix"),prefix.getBytes(), StandardOpenOption.CREATE);
-                }else if(systemId.startsWith("file")){
-                        String prefix=Files.lines(Paths.get(path,"prefix"))
-                                .findAny()
-                                .orElse("http://www.w3.org/TR/xhtml1/DTD/");
-                        systemId=prefix+systemId.substring(systemId.lastIndexOf('/'));
+            String name = systemId.substring(systemId.lastIndexOf('/'));
+            if (!(Paths.get(path, name).toFile().exists())) {
+                if (systemId.startsWith("http")) {
+                    String prefix = systemId.substring(0, systemId.lastIndexOf('/'));
+                    Files.write(Paths.get(path, "prefix"), prefix.getBytes(), StandardOpenOption.CREATE);
+                } else if (systemId.startsWith("file")) {
+                    String prefix = Files.lines(Paths.get(path, "prefix"))
+                            .findAny()
+                            .orElse("http://www.w3.org/TR/xhtml1/DTD/");
+                    systemId = prefix + systemId.substring(systemId.lastIndexOf('/'));
                     //Utility.log("reformed s="+systemId);
                 }
                 Utility.log("fail to find the local schema, going to creat one.");
                 URL website = new URL(systemId);
                 ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-                FileOutputStream fos = new FileOutputStream(path+name);
+                FileOutputStream fos = new FileOutputStream(path + name);
                 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-                Utility.log("scheme is downloaded to "+Paths.get(path,name).toAbsolutePath().toString());
+                Utility.log("scheme is downloaded to " + Paths.get(path, name).toAbsolutePath().toString());
             }
 
-            return new InputSource(new FileInputStream(path+name));
+            return new InputSource(new FileInputStream(path + name));
         }
 
         public List<Hyperlink> getData() {
@@ -97,25 +122,25 @@ public class Index {
         }
     }
 
-    static List<Hyperlink> parse(byte[] source){
-        List<Hyperlink> data=new ArrayList<>();
+    static List<Hyperlink> parse(byte[] source) {
+        List<Hyperlink> data = new ArrayList<>();
         try {
-            SAXParserFactory spf=SAXParserFactory.newInstance();
+            SAXParserFactory spf = SAXParserFactory.newInstance();
             spf.setNamespaceAware(true);
-            SAXParser saxParser=spf.newSAXParser();
+            SAXParser saxParser = spf.newSAXParser();
             //Utility.log("parse alpha");
-            saxParser.parse(new InputSource(new ByteArrayInputStream(source)),new ddReader(data));
+            saxParser.parse(new InputSource(new ByteArrayInputStream(source)), new ddReader(data));
             //Utility.log("parse omega");
-        }catch (IOException|SAXException|ParserConfigurationException e){
+        } catch (IOException | SAXException | ParserConfigurationException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
 
         }
-        //data.forEach(v-> System.out.println(v.getText()+" -> "+v.getHref()));
+        //data.forEach(v -> System.out.println(v.getText() + " -> " + v.getHref()));
         return data;
     }
 
-    private Map<Integer,Hyperlink> data;
+    private Map<Integer, Hyperlink> data;
     private String source;
     private String path;
 
@@ -123,10 +148,10 @@ public class Index {
         this.source = source;
         this.path = path;
 
-        if(!Files.exists(Paths.get(path))){
+        if (!Files.exists(Paths.get(path))) {
             try {
                 Files.createDirectories(Paths.get(path));
-                Files.createFile(Paths.get(path,"index"));
+                Files.createFile(Paths.get(path, "index"));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -135,9 +160,9 @@ public class Index {
         setupLog();
     }
 
-    boolean setupLog(){
-        Path p=Paths.get(getPath(),"index");
-        if(!Files.exists(p)){
+    boolean setupLog() {
+        Path p = Paths.get(getPath(), "index");
+        if (!Files.exists(p)) {
             try {
                 Files.createFile(p);
             } catch (IOException e) {
@@ -148,40 +173,40 @@ public class Index {
         return true;
     }
 
-    boolean process(){
-        data=new TreeMap<>();
-        byte[] src=Utility.download(source);
-        if(src==null){
+    boolean process() {
+        data = new TreeMap<>();
+        byte[] src = Utility.download(source);
+        if (src == null) {
             return false;
         }
-        parse(Utility.clean(src)).forEach(v->{
-            String link=v.getHref();
+        parse(Utility.clean(src)).forEach(v -> {
+            String link = v.getHref();
             //noinspection StatementWithEmptyBody
-            if(link.startsWith("http")){
+            if (link.startsWith("http")) {
                 //like "http://www.fhxiaoshuo.com/read/67/67220/11125326.shtml"
-            }else if(link.startsWith("/")){
+            } else if (link.startsWith("/")) {
                 //like "/read/67/67220/11125326.shtml"
                 try {
-                    URL host=new URL(source);
-                    link=new URL(host.getProtocol(),host.getHost(),link).toString();
+                    URL host = new URL(source);
+                    link = new URL(host.getProtocol(), host.getHost(), link).toString();
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
-            }else{
+            } else {
                 //like "11125326.shtml"
-                link= source +link;
+                link = source + link;
             }
 
-            int code=Integer.valueOf(link.substring(link.lastIndexOf('/')+1,link.lastIndexOf('.')));
+            int code = Integer.valueOf(link.substring(link.lastIndexOf('/') + 1, link.lastIndexOf('.')));
 
-            data.put(code,new Hyperlink(v.getText(),link));
+            data.put(code, new Hyperlink(v.getText(), link));
         });
 
         return true;
     }
 
     Map<Integer, Hyperlink> getData() {
-        if(data==null){
+        if (data == null) {
             process();
         }
         return data;
