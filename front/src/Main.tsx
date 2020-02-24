7b521f8b974b1186ba0a1eec1a6bc7f336ec152d
Alex <alexhyisen@gmail.com>
2020-02-24 20:16:19 +0800

    add Tabs structure

--- front/src/Main.tsx
+++ front/src/Main.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useReducer, useState} from "react";
-import {Button, createStyles, Grid, TextField, Theme} from "@material-ui/core";
+import {AppBar, Button, createStyles, Grid, Tab, Tabs, TextField, Theme, useTheme} from "@material-ui/core";
 import LoopIcon from '@material-ui/icons/Loop';
 import ItemView, {Item, MessageType} from "./components/ItemView";
 import {Service} from "./Service";
 import {unstable_batchedUpdates} from "react-dom";
 import {makeStyles} from "@material-ui/core/styles";
+import {genTabProps, TabClazz, TabPanel} from "./components/TabPanel";
+import SwipeableViews from 'react-swipeable-views';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -13,13 +15,14 @@ const useStyles = makeStyles((theme: Theme) =>
             marginTop: 10,
         },
         root: {
-            padding: theme.spacing(1)
+            padding: theme.spacing(0)
         }
-    }),
+    })
 );
 
 function Main() {
     const classes = useStyles({});
+    const theme = useTheme();
 
     const [host, setHost] = useState("hyisen.net");
     const [port, setPort] = useState(443);
@@ -28,6 +31,8 @@ function Main() {
     const [message, setMessage] = useState("");
 
     const [items, addItem] = useReducer((state: Item[], action: Item) => state.concat(action), []);
+
+    const [value, setValue] = React.useState(TabClazz.Config);
 
     function addMessage(cls: MessageType, msg: string): void {
         addItem({id: items.length, type: cls, message: msg, timestamp: Date.now()});
@@ -55,85 +60,108 @@ function Main() {
 
     return (
         <div>
-            <Grid container className={classes.root} spacing={4}>
-                <Grid item>
-                    <TextField
-                        id="host"
-                        label="host"
-                        value={host}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setHost(event.target.value)}
-                        style={{width: 200}}
-                    />
-                </Grid>
-                <Grid item>
-                    <TextField
-                        id="port"
-                        label="port"
-                        type="number"
-                        value={port}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPort(Number(event.target.value))}
-                        style={{width: 78}}
-                    />
-                </Grid>
-                <Grid item>
-                    <Button className={classes.button}
-                            onClick={() => {
-                                if (linking) {
-                                    return;
-                                }
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={(event, value) => setValue(value)}
+                    variant="fullWidth"
+                    aria-label="full width tabs"
+                >
+                    <Tab {...genTabProps(TabClazz.Config)} />
+                    <Tab {...genTabProps(TabClazz.Reader)} />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={(index) => setValue(index)}
+            >
+                <TabPanel value={value} index={TabClazz.Config} dir={theme.direction}>
+                    <Grid container className={classes.root} spacing={4}>
+                        <Grid item>
+                            <TextField
+                                id="host"
+                                label="host"
+                                value={host}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setHost(event.target.value)}
+                                style={{width: 200}}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                id="port"
+                                label="port"
+                                type="number"
+                                value={port}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPort(Number(event.target.value))}
+                                style={{width: 80}}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button className={classes.button}
+                                    onClick={() => {
+                                        if (linking) {
+                                            return;
+                                        }
 
-                                if (!linked) {
-                                    setLinking(true);
+                                        if (!linked) {
+                                            setLinking(true);
 
-                                    service.link(host, port, true);
-                                    service.addEventListener('open', () => {
-                                        unstable_batchedUpdates(() => {
-                                            // Hope it would be automatically in future.
-                                            setLinked(true);
-                                            setLinking(false);
-                                        });
-                                    }, true);
-                                } else {
-                                    setLinking(true);
-                                    service.addEventListener('close', () => {
-                                        unstable_batchedUpdates(() => {
-                                            setLinked(false);
-                                            setLinking(false);
-                                        });
-                                    }, true);
-                                }
-                            }}
-                            variant="contained"
-                            color={linked ? "primary" : "secondary"}
-                    >{linking ? <LoopIcon/> : linked ? "ON" : "OFF"}</Button>
-                </Grid>
+                                            service.link(host, port, true);
+                                            service.addEventListener('open', () => {
+                                                unstable_batchedUpdates(() => {
+                                                    // Hope it would be automatically in future.
+                                                    setLinked(true);
+                                                    setLinking(false);
+                                                });
+                                            }, true);
+                                        } else {
+                                            setLinking(true);
+                                            service.addEventListener('close', () => {
+                                                unstable_batchedUpdates(() => {
+                                                    setLinked(false);
+                                                    setLinking(false);
+                                                });
+                                            }, true);
+                                        }
+                                    }}
+                                    variant="contained"
+                                    color={linked ? "primary" : "secondary"}
+                            >{linking ? <LoopIcon/> : linked ? "ON" : "OFF"}</Button>
+                        </Grid>
 
-                <Grid item>
-                    <TextField
-                        autoComplete="off"
-                        id="message"
-                        label="message"
-                        value={message}
-                        variant="outlined"
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setMessage(event.target.value)}
-                        style={{width: 295}}
-                    />
-                </Grid>
-                <Grid item>
-                    <Button className={classes.button}
-                            onClick={() => {
-                                addMessage(MessageType.Client, message);
-                                service.send(message);
-                                setMessage("");
-                            }}
-                            variant="contained"
-                            color="primary"
-                    >SEND</Button>
-                </Grid>
-                <Grid item xs={12}>
-                    <ItemView data={items}/>
-                </Grid>
-            </Grid>
+                        <Grid item>
+                            <TextField
+                                autoComplete="off"
+                                id="message"
+                                label="message"
+                                value={message}
+                                variant="outlined"
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setMessage(event.target.value)}
+                                style={{width: 312}}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button className={classes.button}
+                                    onClick={() => {
+                                        addMessage(MessageType.Client, message);
+                                        service.send(message);
+                                        setMessage("");
+                                    }}
+                                    variant="contained"
+                                    color="primary"
+                            >SEND</Button>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <ItemView data={items}/>
+                        </Grid>
+                    </Grid>
+                </TabPanel>
+                <TabPanel value={value} index={TabClazz.Reader} dir={theme.direction}>
+                    Item Two
+                </TabPanel>
+            </SwipeableViews>
+
         </div>
     );
 }
---
+++ front/src/components/TabPanel.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {Box, Typography} from "@material-ui/core";
+
+export enum TabClazz {
+    Config,
+    Reader
+}
+
+function toLabel(clz: TabClazz) {
+    switch (clz) {
+        case TabClazz.Config:
+            return "config";
+        case TabClazz.Reader:
+            return "reader";
+    }
+}
+
+export interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: TabClazz;
+    value: TabClazz;
+}
+
+export function TabPanel(props: TabPanelProps) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
+
+export function genTabProps(index: TabClazz) {
+    return {
+        label: toLabel(index),
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
