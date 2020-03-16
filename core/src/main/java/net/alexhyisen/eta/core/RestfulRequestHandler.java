447209c0e688126c2b7ee9aa56d1fce33b86064f
Alex <alexhyisen@gmail.com>
2020-03-17 04:39:31 +0800

    add the auth api

--- core/src/main/java/net/alexhyisen/eta/core/RestfulRequestHandler.java
+++ core/src/main/java/net/alexhyisen/eta/core/RestfulRequestHandler.java
@@ -9,6 +9,7 @@ import net.alexhyisen.Keeper;
 import net.alexhyisen.Utility;
 import net.alexhyisen.log.LogCls;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
@@ -17,18 +18,20 @@ import java.util.stream.Collectors;
 
 public class RestfulRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     private final String prefix;
-    public static String HEADER_CREDENTIAL_NAME = "credential";
     private final String adminUsername;
     private final String adminPassword;
 
-    public RestfulRequestHandler(String prefix, String adminUsername, String adminPassword) {
+    public static String HEADER_CREDENTIAL_NAME = "credential";
+    private static Keeper keeper = new Keeper();
+
+
+    public RestfulRequestHandler(String prefix, String adminUsername, String adminPassword) throws IOException {
         this.prefix = prefix;
         this.adminUsername = adminUsername;
         this.adminPassword = adminPassword;
+
+        keeper.load();
     }
-
-
-    private static Keeper keeper = new Keeper();
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) {
@@ -54,20 +57,27 @@ public class RestfulRequestHandler extends SimpleChannelInboundHandler<FullHttpR
             } else {
                 Utils.respond(ctx, request, HttpResponseStatus.FORBIDDEN, "application/json", json.getBytes());
             }
+        } else if (uri.equals("/auth") && request.method().equals(HttpMethod.PUT)) {
+            String token = request.headers().get(HEADER_CREDENTIAL_NAME);
+            String json = request.content().toString(StandardCharsets.UTF_8);
+            var credential = new Credential(json);
+            if (!keeper.isAuthorized(token)) {
+                Utils.respond(ctx, request, HttpResponseStatus.FORBIDDEN,
+                        "text/plain", String.format("bad token %s", token).getBytes());
+            } else {
+                try {
+                    keeper.put(credential.getUsername(), credential.getPassword());
+                    Utils.respond(ctx, request, HttpResponseStatus.CREATED,
+                            "application/json", json.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Utils.respond(ctx, request, HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                            "text/plain", e.toString().getBytes());
+                    return;
+                }
+                Utility.log(LogCls.AUTH, String.format("%s add %s", token, credential.getUsername()));
+            }
         }
-    }
-
-    private Optional<String> genCredential(String username, String password) {
-        String ret = null;
-
-        if (adminUsername.equals(username) && adminPassword.equals(password) || keeper.isAuthorized(username, password)) {
-            ret = keeper.register();
-            Utility.log(LogCls.AUTH, String.format("auth %s with %s", username, ret));
-        } else {
-            Utility.log(LogCls.AUTH, String.format("failed to auth %s|%s", username, password));
-        }
-
-        return Optional.ofNullable(ret);
     }
 
     private static class Credential {
@@ -100,5 +110,18 @@ public class RestfulRequestHandler extends SimpleChannelInboundHandler<FullHttpR
         public String getPassword() {
             return password;
         }
+    }
+
+    private Optional<String> genCredential(String username, String password) {
+        String ret = null;
+
+        if (adminUsername.equals(username) && adminPassword.equals(password) || keeper.isAuthorized(username, password)) {
+            ret = keeper.register();
+            Utility.log(LogCls.AUTH, String.format("auth %s with %s", username, ret));
+        } else {
+            Utility.log(LogCls.AUTH, String.format("failed to auth %s|%s", username, password));
+        }
+
+        return Optional.ofNullable(ret);
     }
 }
